35f02af0a17b9b30215cbdb10eb644dd8e578bee
abbykeck <abigailkeck2425@gmail.com>
2025-09-05 18:44:40 +0000

    completed bubblesort.c

--- bubblesort.c
+++ bubblesort.c
@@ -1,3 +1,14 @@
+/*  algorithm for main
+ * 1. create the array that needs to be sorted
+ * 2. print out the array and label it "before"
+ * 3. make variables and fill them with int values for testing the swap function
+ * 4. print these variables
+ * 5. test the swap function by calling it with the test values
+ * 6. print the test values again to make sure the swap function works
+ * 7. call the sort function with the array that needs to be sorted
+ * 8. print out the array and label it "after"
+ * 9. end main 
+ */
 // starter code
 
 #include <stdio.h>
@@ -27,15 +38,57 @@ int main(){
   return(0);
 } // end main
   
-    /*
-     * algorithm for sort function
-     *
-   *constant MAX is max length of array
-function sort (array):
-    create integer variables i and j
-    for i from zero to MAX - 1:
-        for j from zero to MAX - 1:
-            if array[j] > array[j+1]:
-                swap array[j] with array[j+1]
-                printArray(array)
+/*
+ * algorithm for sort function
+ * 0. parameter: int pointer
+ * 1. for i from zero to MAX - 1:
+       i. for j from zero to MAX - 1:
+           a. if array[j] > array[j+1]:
+              1. swap array[j] with array[j+1]
+              2. printArray(array)
+	   b. end for j
+       ii. end for i
+ * 2. end sort
 */
+void sort(int* array) {
+	for (int i = 0; i < MAX; i++) {
+		for (int j = 0; j < MAX; j++) {
+			if (array[j] > array[j + 1]) {
+				swap(array + j, array + j + 1);
+				printValues(array);
+			} // end if
+		} // end for j
+	} // end for i
+} // end sort
+
+/* algorithm for swap function
+ * 0. parameters: int pointer a, int pointer b
+ * 1. create an int variable to temporarily hold one of the values
+ * 2. assign the value at a to temp
+ * 3. assign the value at b to where a is
+ * 4. assign the value of temp to where b is
+ * 5. end swap
+*/
+void swap(int* a, int* b) {
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+} // end swap
+
+/* algorithm for printValues function
+ * 0. parameter: int pointer
+ * 1. print a bracket
+ * 2. for every element in array from i = 0 to i = length - 1
+ * 	i. print the element at index i and a space
+ * 3. end for
+ * 4. print a closing bracket
+ * 5. end printValues
+*/
+void printValues(int* array) {
+	printf("[");
+	for (int i = 0; i < MAX; i++) {
+		printf("%d ", array[i]);
+	} // end for
+	printf("] \n");
+} // end printValues
